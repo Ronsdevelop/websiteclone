b7dec64e181480d8e1bf1d4cb9d41aa374650f45
Ronsdevelop <rony_aguilera@hotmail.com>
2021-04-05 13:55:12 -0500

    05-04-2021

--- verde/js/main.js
+++ verde/js/main.js
@@ -1,26 +1,35 @@
 /*================== MOSTRAR MENU ===================*/
 
-const mostrarMenu = (toggleId, navId) =>{
+const mostrarMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
-    const mq768 = window.matchMedia('screen and (max-width: 768px)');
-     
-    
+        nav = document.getElementById(navId), mask = document.getElementById('mask');
+
+
+
     // Validar si existe la variable
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('mostrar-menu');
-            if (mq768.matches) {
-                toggle.style.display = 'none';
-            }
-            
+            mask.classList.add('mask-overlay');
+
+
         })
     }
-   
+    const closeMenu = document.querySelector('#nav-menu .close-menu');
+    closeMenu.addEventListener('click', () => {
+        nav.classList.remove('mostrar-menu');
+        mask.classList.remove('mask-overlay');
+
+    });
+
 
 };
-mostrarMenu('nav-toggle','nav-menu');
+mostrarMenu('nav-toggle', 'nav-menu');
+
+/* MENU MOBIL */
+
+
 
 /*  CERRAR MENU */
 /* const mainMenu = document.querySelector();
@@ -35,40 +44,41 @@ function close(){
 /*==================== REMOVER EL MENU MOVIL ====================*/
 const navLink = document.querySelectorAll('.nav-enlaces')
 
-function linkAction(){
-    navLink.forEach(n=>n.classList.remove('activo'));
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('activo'));
     this.classList.add('activo');
-  /*   remover el menu en responsive */
+    /*   remover el menu en responsive */
     const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('mostrar-menu')
-    
+
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('seccion[id]');
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         Id = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav-menu a[href*=' + Id + ']').classList.add('activo')
-        }else{
+        } else {
             document.querySelector('.nav-menu a[href*=' + Id + ']').classList.remove('activo')
         }
     })
 }
 window.addEventListener('scroll', scrollActive);
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
-function scrollHeader(){
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
     const nav = document.getElementById('header')
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if (this.scrollY >= 200) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader);
